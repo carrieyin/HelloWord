4da6dce0902a560aa082230400b27fbc7d1b6c0b
yindd <695685009@qq.com>
2019-04-21 23:14:49 +0800

    自定义类加载器

---
+++ src/main/java/reflect/ydd/demo/ClassLoaderDemo.java
@@ -0,0 +1,34 @@
+package reflect.ydd.demo;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class ClassLoaderDemo {
+
+	public static void main(String[] args) throws MalformedURLException, ClassNotFoundException {
+		String path = "file:/F:/code/classes/";
+		String clzName = "reflect.ydd.demo.Some";
+		System.out.println(path);
+		
+		Class clz1 = loadClassFrom(path, clzName);
+		System.out.println(clz1);		
+		
+		Class clz2 = loadClassFrom(path, clzName);
+		System.out.println(clz2);
+		
+		String ttt = clz1.getClassLoader().getClass().getName() ;
+		System.out.println("ttt: " + ttt);
+		String ttt2 = clz2.getClassLoader().getClass().getName() ;
+		System.out.println("ttt2: " + ttt2);
+		
+		System.out.printf("clz1 与 clz2 为 %s 实例", clz1 == clz2 ? "相同":"不同");
+		
+	}
+
+	private static Class loadClassFrom(String path, String clzName) throws MalformedURLException, ClassNotFoundException {
+		URLClassLoader urlClassLoader = new URLClassLoader(new URL[]{new URL(path)});
+		return urlClassLoader.loadClass(clzName);
+	}
+
+}
