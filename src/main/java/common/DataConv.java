06069be9567152c34694a52fa8a4e19656240b2c
yindd <695685009@qq.com>
2019-05-09 22:44:41 +0800

    java基础

---
+++ src/main/java/common/DataConv.java
@@ -0,0 +1,22 @@
+package common;
+
+public class DataConv {
+
+	public static void main(String[] args) {
+		//byte a = 200; //超出范围
+		byte a = 20;
+		System.out.println(a);
+		System.out.println(1 +2 +3 + "hello " + 1 + 2 + 3); //+用于字符串为连接符
+		System.out.println(10 % -3);
+		System.out.println(-10 % -3); //取决于被除数的正负
+		int i = 0;
+		i = i++;
+		System.out.println(i);
+		
+		
+		int c = 0; //0  1
+		//int sum = ++c;	//c = c + 1; sum = a;
+		int sum1 = c++;  //sum = 0; c = c + 1;
+		System.out.println(sum1);
+	}
+}
