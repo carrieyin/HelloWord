4384243efe4f1cfbf37dcf99344e473dd9b7baef
yindd <695685009@qq.com>
2019-05-14 19:40:12 +0800

    进制转换 继承
    Signed-off-by: yindd <695685009@qq.com>

--- src/main/java/Some.java
+++ src/main/java/Some.java
@@ -1,4 +1,7 @@
-
-public class Some {
-
+class Helloworld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hello world");
+	}
 }
--- src/main/java/com/ydd/demo/common/DataConv.java
+++ src/main/java/com/ydd/demo/common/DataConv.java
@@ -1,4 +1,4 @@
-package common;
+package com.ydd.demo.common;
 
 public class DataConv {
 
