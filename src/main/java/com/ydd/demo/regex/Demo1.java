9803eb5f54367bdc7704852de6c2a6428c27b983
yindd <695685009@qq.com>
2019-07-02 22:00:00 +0800

    正则表达式

---
+++ src/main/java/com/ydd/demo/regex/Demo1.java
@@ -0,0 +1,33 @@
+package com.ydd.demo.regex;
+
+import java.util.Arrays;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		//-------------------预定义字符------------------------ 
+		System.out.println("%1".matches("."));
+		System.out.println("%".matches("."));
+		System.out.println("数字字符:" + "2".matches("\\d"));
+		System.out.println("非数字字符:" + "%".matches("\\d"));
+		System.out.println("空白字符:" + "\r".matches("\\s"));
+		System.out.println("空白字符:" + "\r".matches("\\s"));
+		System.out.println("单词字符:" + "_".matches("\\w"));
+		//-------------------数量词----------------------------
+		System.out.println("?:0次或1次" + "_".matches("\\w?"));
+		System.out.println("*:0次或多次" + "_".matches("\\w*"));
+		System.out.println("+:1次或多次" + "_".matches("\\w*"));
+		System.out.println("+:两者之间" + "_".matches("\\w{2,3}"));
+		//-------------------范围词-----------------------------
+		System.out.println("abc".matches("[abc]{3}"));
+		
+		//-------------------切割-------------------------------
+		String str = "明天   放假   了";
+		String[] arr = str.split(" +");
+		System.out.println(Arrays.toString(arr));
+		
+		//-------------------分组-------------------------------
+		String str1 = "明天天  --- 放假假   了";
+		String[] arr1 = str1.split("(.)\\1+");
+		System.out.println(Arrays.toString(arr1));
+	}
+}
